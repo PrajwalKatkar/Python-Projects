f6de68feddbca3a46236b10d643cc7abe0a36c02
Prajwal Katkar <prajwalkatkar17@gmail.com>
2026-02-15 10:28:37 +0530

    Change speech recognition from Sphinx to Google
    Updated speech recognition method from Sphinx to Google.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import speech_recognition as sr
 import webbrowser 
 import pyttsx3 
-
+#pip install pocketsphinx
 recognizer = sr.Recognizer()
 engine=pyttsx3.init()
 
@@ -20,10 +20,8 @@ if __name__=="__main__":
             print("Say something!")
             audio = r.listen(source)
 
-        # recognize speech using Sphinx
         try:
-            print("Sphinx thinks you said " + r.recognize_sphinx(audio))
-        except sr.UnknownValueError:
-            print("Sphinx could not understand audio")
-        except sr.RequestError as e:
+            command=r.recognize_google(audio)
+            print(command)
+        except Exception as e:
             print("Sphinx error; {0}".format(e))
