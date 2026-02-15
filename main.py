f60b36a58434e7b276b68cfc751985cae256cb73
Prajwal Katkar <prajwalkatkar17@gmail.com>
2026-02-15 09:56:41 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import speech_recognition as sr
+import webbrowser 
+import pyttsx3 
+
+recognizer = sr.Recognizer()
+engine=pyttsx3.init()
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+if __name__=="__main__":
+    speak("Initializing Jarvis.......")
+    while True:
+        #listen for wake word jarvis
+
+        # obtain audio from the microphone
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Say something!")
+            audio = r.listen(source)
+
+        # recognize speech using Sphinx
+        try:
+            print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+        except sr.UnknownValueError:
+            print("Sphinx could not understand audio")
+        except sr.RequestError as e:
+            print("Sphinx error; {0}".format(e))
