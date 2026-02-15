8f1821f29c2c015fab7b35ed180dfbfe54461b5d
Prajwal Katkar <prajwalkatkar17@gmail.com>
2026-02-15 10:51:05 +0530

    Refactor Jarvis initialization and command handling
    Added command processing and improved audio listening logic.

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import pyttsx3
 #pip install pocketsphinx
 recognizer = sr.Recognizer()
 engine=pyttsx3.init()
-
+def processcommand(c):
+    pass
 def speak(text):
     engine.say(text)
     engine.runAndWait()
@@ -16,12 +17,19 @@ if __name__=="__main__":
 
         # obtain audio from the microphone
         r = sr.Recognizer()
-        with sr.Microphone() as source:
-            print("Say something!")
-            audio = r.listen(source)
+    
 
         try:
-            command=r.recognize_google(audio)
-            print(command)
+            with sr.Microphone() as source:
+                print("Listening")
+                audio = r.listen(source,timeout=2,phrase_time_limit=1)
+            word=r.recognize_google(audio)
+            if (word.lower()=="jarvis"):
+                speak("Yaa")
+                with sr.Microphone() as source:
+                    print("Jarvis Active")
+                    audio = r.listen(source)
+                    command=r.recognize_google(audio)
+                    processcommand()
         except Exception as e:
             print("Sphinx error; {0}".format(e))
