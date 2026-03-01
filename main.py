1528cc92f24e13104696269108895b3edb608360
Prajwal Katkar <prajwalkatkar17@gmail.com>
2026-03-02 01:31:08 +0530

    Refactor Jarvis commands and improve functionality

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ import pyttsx3
 #pip install pocketsphinx
 recognizer = sr.Recognizer()
 engine=pyttsx3.init()
-def processcommand(c):
-    pass
 def speak(text):
     engine.say(text)
     engine.runAndWait()
+def processcommand(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://google.com")
+    if "open youtube" in c.lower():
+        webbrowser.open("https://youtube.com")
+    if "open chatgpt" in c.lower():
+        webbrowser.open("https://chatgpt.com")
+    if "open github" in c.lower():
+        webbrowser.open("https://github.com/PrajwalKatkar")
 
 if __name__=="__main__":
     speak("Initializing Jarvis.......")
@@ -17,19 +24,19 @@ if __name__=="__main__":
 
         # obtain audio from the microphone
         r = sr.Recognizer()
-    
-
         try:
             with sr.Microphone() as source:
                 print("Listening")
                 audio = r.listen(source,timeout=2,phrase_time_limit=1)
             word=r.recognize_google(audio)
             if (word.lower()=="jarvis"):
-                speak("Yaa")
+                speak("Yeah What")
                 with sr.Microphone() as source:
                     print("Jarvis Active")
                     audio = r.listen(source)
                     command=r.recognize_google(audio)
-                    processcommand()
+                    processcommand(command)
         except Exception as e:
             print("Sphinx error; {0}".format(e))
+
+
